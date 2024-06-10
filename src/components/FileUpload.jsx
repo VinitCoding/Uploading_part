4b10f31d45c08ce34da9ae3d6ab2558517601708
VinitCoding <vinitcoding1001@gmail.com>
2024-06-10 18:00:10 +0530

    commit

--- src/components/FileUpload.jsx
+++ src/components/FileUpload.jsx
@@ -14,18 +14,26 @@ const FileUpload = () => {
     // HandleChange function
     const handleChange = (e) => {
         if (e.target.files && e.target.files.length) {
-            setSelectedFile(e.target.files)
+            setSelectedFile(e.target.files[0])
+            console.log(selectedFile);
         }
     }
 
     // HandleClick Function
     const handleClick = () => {
         inputRef.current.click()
-        // console.log(selectedFile);
+    }
+
+    // clearSelected function
+    const clearSelected = () => {
+        inputRef.current.value = ""
+        setSelectedFile(null)
+        setProgress(0)
+        setFileStatus('selected')
     }
 
     return (
-        <div className='bg-white h-[450px] w-[650px] rounded- text-center flex flex-col items-center shadow-md mt-3'>
+        <div className={`bg-white h-[450px] w-[650px] rounded- text-center flex flex-col items-center shadow-md mt-3`}>
             <h2 className='mt-5 text-[35px] font-semibold text-[#26252D]'>Upload File</h2>
             <input ref={inputRef} type="file" onChange={handleChange} style={{ display: 'none' }} />
 
@@ -41,21 +49,21 @@ const FileUpload = () => {
 
             {/* Showing info about the file */}
             {
-                selectedFile && (<>
-                <div className='flex justify-center items-center px-5 py-3 rounded-lg gap-x-4 bg-[#F8F7F7] mt-5'>
-                    <FaFileAlt className='text-blue-700 text-lg' />
-                    {/* File Name */}
-                    <div className='flex justify-center items-center gap-x-1'>
-                        <h5>File name here</h5>
-                        <div>
-                            <div className='bg-blue-500 ' style={{ width: '40%' }}></div>
+                selectedFile && (<div className='mb-10'>
+                    <div className='flex  overflow-y-auto justify-center items-center px-5 py-3 rounded-lg gap-x-5 bg-[#F8F7F7] mt-5'>
+                        <FaFileAlt className='text-[#3884c2] text-xl' />
+                        {/* File Name */}
+                        <div className='flex flex-col justify-center items-center gap-x-1'>
+                            <h3 className='text-[13px]'>{selectedFile.name}</h3>
+                            <div className='w-full h-[5px] bg-[rgba(0,0,0,0.076)] rounded-[8px] mt-1'>
+                                <div className='w-0 h-[5px] bg-[#3691dc] rounded-[8px] ' style={{ width: `${progress}%` }}></div>
+                            </div>
                         </div>
-                        <button onClick={() => { }}><IoMdClose /></button>
-                    </div>
-                </div>
+                        <button onClick={clearSelected} className='rounded-full bg-[#d0e2fe] p-2 text-center'><IoMdClose className='text-[#3378b0]'/></button>
 
-                <button className=''>Upload</button>
-                </>
+                    </div>
+                    <button className='mt-6 bg-blue-500 px-3 py-2 rounded text-white font-normal'>Upload</button>
+                </div>
 
                 )
             }
