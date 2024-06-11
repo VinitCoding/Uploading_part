8d12597a2b026296a4cf88427f6bfba985d8f761
VinitCoding <vinitcoding1001@gmail.com>
2024-06-11 20:55:57 +0530

    commit

--- frontend/src/components/FileUpload.jsx
+++ frontend/src/components/FileUpload.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import cloud_upload from '../assets/upload_to_cloud.svg'
 import { FaFileAlt } from "react-icons/fa";
 import { IoMdClose } from "react-icons/io";
@@ -10,12 +10,23 @@ const FileUpload = () => {
     // State variables for tracking file status
     const [selectedFile, setSelectedFile] = useState(null)
     const [progress, setProgress] = useState(100);
-    const [fileStatus, setFileStatus] = useState('selected') // setting the file according to file status ['selected'/'uploading'/'done']
+    const [message, setMessage] = useState('')
+    const [loading, setLoading] = useState(false)
 
     // HandleChange function
     const handleChange = (e) => {
-        if (e.target.files && e.target.files.length) {
-            setSelectedFile(e.target.files[0])
+        const selectedFile = e.target.files[0]
+        if (selectedFile) {
+            const fileType = selectedFile.name.split('.').pop().toLowerCase()
+
+            if (fileType === 'xls' || fileType === 'csv' || fileType === 'xlsx') {
+                setSelectedFile(selectedFile)
+                console.log(selectedFile);
+                setMessage('')
+            } else {
+                setSelectedFile(null)
+                setMessage('Please upload an excel file (.xls, .csv or .xlsx)')
+            }
         }
     }
 
@@ -29,30 +40,41 @@ const FileUpload = () => {
         inputRef.current.value = ""
         setSelectedFile(null)
         setProgress(0)
-        setFileStatus('selected')
     }
 
     // handleUpload Function
-    const handleUpload = async() => {
-        if(fileStatus === 'done'){
-            clearSelected()
-            return
+    const handleUpload = async () => {
+        if (!selectedFile) return 
+        setLoading(false)
+        let formData = new FormData()
+        formData.append('file', selectedFile)
+        try {
+            const response = await axios.post('http://127.0.0.1:8000/qualify_leads', formData, {
+                onUploadProgress: (progressEvent) => {
+                    const { load, total } = progressEvent
+                    const progressValue = Math.round((load * 100) / total)
+                    setProgress(progressValue)
+                    
+                    
+                },
+                
+            })
+            if(response){
+                setSelectedFile(null)
+                setLoading(false)
+            }
+            console.log(response.data);
+            
+
+        } catch (error) {
+            console.log('Error while uploading', error);
         }
         try {
-            // Uploading Status
-            setFileStatus('uploading')
-
-            const response = await axios.post('http://localhost:5173/api/upload', {
-                onUploadProgress: (progressEvent) => {
-                    const progressCompleted = Math.round(
-                        (progressEvent.loaded * 100) / progressEvent.total
-                    )
-
-                    setProgress(progressCompleted)
-                }
+            const response = await fetch('http://127.0.0.1:8000/qualify_leads', {
+                method: "POST",
+                body: formData,
             })
-
-            setFileStatus('Done')
+            console.log(response.data);
         } catch (error) {
             console.log('Error while uploading', error);
         }
@@ -67,17 +89,21 @@ const FileUpload = () => {
 
             {/* Button to trigger the file input dialog */}
             {
-                !selectedFile && (
-                    <button className='bg-[#F8F7F7] mt-4 px-36 pb-16 pt-8 border-[2px] border-blue-400 border-dashed rounded-md text-center' onClick={handleClick}>
-                        <img src={cloud_upload} alt="cloud_image " className='mx-auto' />
-                        <h4 className='text-[22px] font-semibold text-[#175195]'>Click to Upload File</h4>
-                        <p className='text-[#3C3C3C] text-[15px] font-semibold mt-3'>Supported File Formats: <span className='text-[#989898] '>.xls, .csv</span></p>
-                    </button>)
+                (!selectedFile) && (
+                    <>
+                        <div className='bg-[#F8F7F7] mt-4 px-36 pb-16 pt-8 border-[2px] border-blue-400 border-dashed rounded-md text-center cursor-pointer' onClick={handleClick}>
+                            <img src={cloud_upload} alt="cloud_image " className='mx-auto' />
+                            <h4 className='text-[22px] font-semibold text-[#175195]'>Click to Upload File</h4>
+                            <p className='text-[#3C3C3C] text-[15px] font-semibold mt-3'>Supported File Formats: <span className='text-[#989898] '>.xls, .csv, .xlsx</span></p>
+
+                        </div>
+                        <p className='pb-3 mt-1 text-base text-red-500'>{message}</p>
+                    </>)
             }
 
             {/* Showing info about the file */}
             {
-                selectedFile && (<div className='mb-10'>
+                (selectedFile || loading) && (<div className='mb-10'>
                     <div className='flex  overflow-y-auto justify-center items-center px-5 py-3 rounded-lg gap-x-5 bg-[#F8F7F7] mt-5'>
                         <FaFileAlt className='text-[#3884c2] text-xl' />
                         {/* File Name */}
@@ -87,7 +113,7 @@ const FileUpload = () => {
                                 <div className='w-0 h-[5px] bg-[#3691dc] rounded-[8px] ' style={{ width: `${progress}%` }}></div>
                             </div>
                         </div>
-                        <button onClick={clearSelected} className='rounded-full bg-[#d0e2fe] p-2 text-center'><IoMdClose className='text-[#3378b0]'/></button>
+                        <button onClick={clearSelected} className='rounded-full bg-[#d0e2fe] p-2 text-center'><IoMdClose className='text-[#3378b0]' /></button>
 
                     </div>
                     <button className='px-3 py-2 mt-6 font-normal text-white bg-blue-500 rounded' onClick={handleUpload}>Upload</button>
