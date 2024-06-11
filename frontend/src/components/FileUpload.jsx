84e18e4c195f0ddd9669a23cde635e2eead8eb55
VinitCoding <vinitcoding1001@gmail.com>
2024-06-11 11:17:54 +0530

    commit

--- frontend/src/components/FileUpload.jsx
+++ frontend/src/components/FileUpload.jsx
@@ -2,20 +2,20 @@ import React, { useRef, useState } from 'react'
 import cloud_upload from '../assets/upload_to_cloud.svg'
 import { FaFileAlt } from "react-icons/fa";
 import { IoMdClose } from "react-icons/io";
+import axios from 'axios';
 
 const FileUpload = () => {
     const inputRef = useRef()
 
     // State variables for tracking file status
     const [selectedFile, setSelectedFile] = useState(null)
-    const [progress, setProgress] = useState(0);
+    const [progress, setProgress] = useState(100);
     const [fileStatus, setFileStatus] = useState('selected') // setting the file according to file status ['selected'/'uploading'/'done']
 
     // HandleChange function
     const handleChange = (e) => {
         if (e.target.files && e.target.files.length) {
             setSelectedFile(e.target.files[0])
-            console.log(selectedFile);
         }
     }
 
@@ -32,8 +32,36 @@ const FileUpload = () => {
         setFileStatus('selected')
     }
 
+    // handleUpload Function
+    const handleUpload = async() => {
+        if(fileStatus === 'done'){
+            clearSelected()
+            return
+        }
+        try {
+            // Uploading Status
+            setFileStatus('uploading')
+
+            const response = await axios.post('http://localhost:5173/api/upload', {
+                onUploadProgress: (progressEvent) => {
+                    const progressCompleted = Math.round(
+                        (progressEvent.loaded * 100) / progressEvent.total
+                    )
+
+                    setProgress(progressCompleted)
+                }
+            })
+
+            setFileStatus('Done')
+        } catch (error) {
+            console.log('Error while uploading', error);
+        }
+    }
+
+
+
     return (
-        <div className={`bg-white h-[450px] w-[650px] rounded- text-center flex flex-col items-center shadow-md mt-3`}>
+        <div className={`bg-white max-h-[450px] min-h-fit  w-[650px] rounded-md text-center flex flex-col items-center shadow-md mt-3 pb-10`}>
             <h2 className='mt-5 text-[35px] font-semibold text-[#26252D]'>Upload File</h2>
             <input ref={inputRef} type="file" onChange={handleChange} style={{ display: 'none' }} />
 
@@ -53,7 +81,7 @@ const FileUpload = () => {
                     <div className='flex  overflow-y-auto justify-center items-center px-5 py-3 rounded-lg gap-x-5 bg-[#F8F7F7] mt-5'>
                         <FaFileAlt className='text-[#3884c2] text-xl' />
                         {/* File Name */}
-                        <div className='flex flex-col justify-center items-center gap-x-1'>
+                        <div className='flex flex-col items-center justify-center gap-x-1'>
                             <h3 className='text-[13px]'>{selectedFile.name}</h3>
                             <div className='w-full h-[5px] bg-[rgba(0,0,0,0.076)] rounded-[8px] mt-1'>
                                 <div className='w-0 h-[5px] bg-[#3691dc] rounded-[8px] ' style={{ width: `${progress}%` }}></div>
@@ -62,7 +90,7 @@ const FileUpload = () => {
                         <button onClick={clearSelected} className='rounded-full bg-[#d0e2fe] p-2 text-center'><IoMdClose className='text-[#3378b0]'/></button>
 
                     </div>
-                    <button className='mt-6 bg-blue-500 px-3 py-2 rounded text-white font-normal'>Upload</button>
+                    <button className='px-3 py-2 mt-6 font-normal text-white bg-blue-500 rounded' onClick={handleUpload}>Upload</button>
                 </div>
 
                 )
