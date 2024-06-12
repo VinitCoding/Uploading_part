614d7ee0b6578d3b8f3f81afa2f7fdc3465a0501
VinitCoding <vinitcoding1001@gmail.com>
2024-06-12 12:11:05 +0530

    commit

--- frontend/src/pages/DisplayData.jsx
+++ frontend/src/pages/DisplayData.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import Upload from '../components/Upload'
+import CraftedLeads from '../components/CraftedLeads'
 
 const DisplayData = () => {
   return (
     <div className='bg-[#AED8FF] w-full h-full px-6 border'>
         <Upload />
+
+        <div className='flex justify-between flex-initial mt-12'>
+            {/* Crafted Leads */}
+            <div className='bg-white w-[600px] h-[200px]'>
+                <h3>Crafted Leads</h3>
+                <CraftedLeads />
+            </div>
+
+            {/* Graphs */}
+            <div className='bg-white w-[600px] h-[200px]'>
+                <h3>Graphs</h3>
+            </div>
+        </div>
     </div>
   )
 }
