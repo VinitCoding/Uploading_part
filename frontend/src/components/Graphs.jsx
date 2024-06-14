b60e9e8fc6e7a8d9bc44d32ec4ade92b354b7b99
VinitCoding <vinitcoding1001@gmail.com>
2024-06-14 14:32:05 +0530

    material tailwind css installed

--- frontend/src/components/Graphs.jsx
+++ frontend/src/components/Graphs.jsx
@@ -1,21 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Graphs = ({ data }) => {
     console.log('Data came to graphs', data.image_list);
 
-    const openModel = (number) => {
-        console.log(`hii ${number}`);
-    }
+    const [openModal, setOpenModal] = useState(false)
 
+    const [showdata, setShowData] = useState({})
+
+    const handleModal = (number) => {
+        setOpenModal(true)
+        setShowData(data.image_list[number])
+    }
     return (
-        <section className='grid grid-cols-2 mt-3.5 gap-6'>
+        <section className='grid grid-cols-2 mt-3.5 gap-6 '>
 
             {
                 data.image_list.map((item, index) => (
-                    <div className='p-1 bg-white rounded-md shadow-md w-fit h-fit' key={index} onClick={() => openModel(index)}>
+                    <div className='p-1 bg-white rounded-md shadow-md w-fit h-fit' key={index} onClick={() => handleModal(index)}>
                         <img src={`data:image/png;base64,${item}`} alt="graphs" />
                     </div>
                 ))
+            }
+            {
+                openModal ? (
+                    <div className=''>
+                        <img src={`data:image/png;base64,${showdata}`} alt="image" />
+                    </div>
+                ) : (null)
             }
         </section>
     )
